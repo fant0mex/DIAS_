2ba5b82b4ff2d91967249028fc02c713744e72d7
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-02-21 10:39:13 +0000

    contact changes

--- page.php
+++ page.php
@@ -1,47 +1,33 @@
 <?php get_header(); ?>
 
- <div class= "container">
-  <div class="row page">
-
-
-
-
-     <div class="col-md-6">
-
-      <?php
-      if ( have_posts() ) :
-        while ( have_posts() ):
-          the_post();
-      ?>
-          <h3 class="page-head"><?php the_title(); ?></h3>
-
-
-          <p><?php the_content(); ?></p>
-
-
-
-      <?php
-        endwhile;
-      else:
-      ?>
-        <p>Sorry, this page does not exist.</p>
-
-      <?php endif; ?>
-
-    </div>
-
-    <div class="col-md-6">
-
-      <?php if ( is_page( 'contact') ) {
+    <?php if ( is_page( 'contact') ) {
 
       get_template_part('content', 'map');
     }
       ?>
 
+  <div class="container">
 
-     </div>
-  </div>
-</div>
+     <?php
+      if ( have_posts() ) :
+        while ( have_posts() ):
+          the_post();
+      ?>
+
+      <h2><?php the_title(); ?></h2>
+
+        <p><?php the_content(); ?></p>
+
+        <?php
+          endwhile;
+           else:
+        ?>
+          <p>Sorry, this page does not exist.</p>
+
+        <?php endif; ?>
+
+    </div>
+
 
 
 
