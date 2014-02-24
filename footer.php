20e95e80817ea291a2b812eebcf851a014e42557
pire <rob.mccormack@diascreative.com>
2014-02-24 14:57:53 +0000

    AMEND: header and footer indentation

--- footer.php
+++ footer.php
@@ -1,37 +1,31 @@
+    <div class= "navbar navbar-default navbar-fixed-bottom">
+      <div class="container-fluid">
+        <div class="navbar-text pull-left">
+          <a href="#">&copy; <?php echo date( 'Y' ) ;?> <?php bloginfo('name'); ?></a>
+        </div>
 
-      <div class= "navbar navbar-default navbar-fixed-bottom">
-          <div class="container-fluid">
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="largeIcon fa fa-facebook"></i></a>
+          <a href="#"><i class="largeIcon fa fa-twitter"></i></a>
+          <a href="#"><i class="largeIcon fa fa-instagram"></i></a>
+          <a href="#"><i class="largeIcon fa fa-linkedin"></i></a>
+          <a href="#"><i class="largeIcon fa fa-pinterest"></i></a>
+        </div>
+      </div>
+    </div>
 
+    <script type="text/javascript">
+      jQuery(document).ready(function($) {
+          $('.carousel').carousel({
+            interval: 2000
+          })
+      });
+    </script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/leaflet/js/leaflet.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/leaflet/js/dias.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/chart/Chart.js"></script>
 
-                <div class="navbar-text pull-left">
-                  <a href="#">&copy; <?php echo date( 'Y' ) ;?> <?php bloginfo('name'); ?></a>
-                </div>
+    <?php wp_footer(); ?>
 
-                 <div class="navbar-text pull-right">
-                  <a href="#"><i class="largeIcon fa fa-facebook"></i></a>
-                  <a href="#"><i class="largeIcon fa fa-twitter"></i></a>
-                  <a href="#"><i class="largeIcon fa fa-instagram"></i></a>
-                  <a href="#"><i class="largeIcon fa fa-linkedin"></i></a>
-                  <a href="#"><i class="largeIcon fa fa-pinterest"></i></a>
-
-                </div>
-
-              </div>
-            </div>
-          </div>
-          <script type="text/javascript">
-              jQuery(document).ready(function($) {
-                  $('.carousel').carousel({
-                    interval: 2000
-                  })
-              });
-              </script>
-              <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/leaflet/js/leaflet.js"></script>
-              <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/leaflet/js/dias.js"></script>
-              <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/chart/Chart.js"></script>
-
-
-           <?php wp_footer(); ?>
-
-       </body>
-      </html>
+  </body>
+</html>
--- header.php
+++ header.php
@@ -1,52 +1,40 @@
-
 <!DOCTYPE html>
-
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="description" content="We make things worth making. Digital + Motion + Interaction.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
+
     <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/style.css" media="all" />
     <link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/images/avatar_50fb2d27989a_128.png">
 
-    <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
-    <meta name="description" content="We make things worth making. Digital + Motion + Interaction.">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
     <?php wp_enqueue_script("jquery"); ?>
-
-
-
     <?php wp_head(); ?>
-
-
-
   </head>
 
   <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
 
-    <!-- Fixed Nav -->
+          <ul id="home">
+             <li><h1 class="logo"><a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/dias_logo.png"></a></h1></li>
+             <li><h1 class="strap"><img src="<?php bloginfo('template_directory'); ?>/images/strapline.png"></h1></li>
+          </ul>
+        </div>
 
-      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <ul id="home">
-               <li><h1 class="logo"><a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/dias_logo.png"></a></h1></li>
-               <li><h1 class="strap"><img src="<?php bloginfo('template_directory'); ?>/images/strapline.png"></h1></li>
-            </ul>
-          </div>
-
-          <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-
-              <li><?php wp_nav_menu( array('menu' => 'Primary' )); ?></li>
-
-            </ul>
-          </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><?php wp_nav_menu( array('menu' => 'Primary' )); ?></li>
+          </ul>
         </div>
       </div>
+    </div>
