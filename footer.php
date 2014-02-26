d51b6d2a2406e2e2e09590698a561d05ed54d9ef
pire <rob.mccormack@diascreative.com>
2014-02-26 16:07:28 +0000

    social links in footer

--- footer.php
+++ footer.php
@@ -5,11 +5,9 @@
         </div>
 
         <div class="navbar-text pull-right">
-          <a href="#"><i class="largeIcon fa fa-facebook"></i></a>
-          <a href="#"><i class="largeIcon fa fa-twitter"></i></a>
-          <a href="#"><i class="largeIcon fa fa-instagram"></i></a>
-          <a href="#"><i class="largeIcon fa fa-linkedin"></i></a>
-          <a href="#"><i class="largeIcon fa fa-pinterest"></i></a>
+          <a href="https://www.facebook.com/DIAScreative"><i class="largeIcon fa fa-facebook"></i></a>
+          <a href="https://twitter.com/DIASCreative"><i class="largeIcon fa fa-twitter"></i></a>
+          <a href="http://www.linkedin.com/company/dias-creative"><i class="largeIcon fa fa-linkedin"></i></a>
         </div>
       </div>
     </footer>
