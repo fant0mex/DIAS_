726f75b6e44435e9b323ddc7a786a49cdfa3dfa8
pire <rob.mccormack@diascreative.com>
2014-02-26 16:54:00 +0000

    marker in map

--- leaflet/js/dias.js
+++ leaflet/js/dias.js
@@ -17,6 +17,8 @@
     L.tileLayer('http://{s}.tile.cloudmade.com/a0750f484fc24abc9d141c74cba0613a/997/256/{z}/{x}/{y}.png', {
         maxZoom: 18
     }).addTo(map);
+
+    var marker = L.marker([51.522555, -0.085304]).addTo(map);
   }
 
   if( $chart.length ) {
