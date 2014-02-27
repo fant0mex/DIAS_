03564e08e452040b5971d224e89921841e492f1a
pire <rob.mccormack@diascreative.com>
2014-02-27 17:47:21 +0000

    proper alignment

--- single-work.php
+++ single-work.php
@@ -10,17 +10,19 @@
     while ( have_posts() ):
       the_post();
   ?>
-    <article class="blog-post work-post textual">
-      <h2><?php the_title(); ?></h2>
+    <div class="col-sx-12">
+      <article class="blog-post work-post textual">
+        <h2><?php the_title(); ?></h2>
 
-      <div class="info">
-        <?php the_time('F jS, Y'); ?> by <?php the_author( );?>
-      </div>
+        <div class="info">
+          <?php the_time('F jS, Y'); ?> by <?php the_author( );?>
+        </div>
 
-      <div class="main-text">
-        <?php the_content(); ?>
-      </div>
-    </article>
+        <div class="main-text">
+          <?php the_content(); ?>
+        </div>
+      </article>
+    </div>
   <?php
     endwhile;
   endif;
