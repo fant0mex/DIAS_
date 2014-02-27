435dd9f9a409776ff8286083adc8001f9a1bcc35
pire <rob.mccormack@diascreative.com>
2014-02-27 17:44:31 +0000

    simple work page

--- single-work.php
+++ single-work.php
@@ -1,4 +1,8 @@
-<?php get_header(); ?>
+<?php
+  get_header();
+  get_template_part('content', 'hero-image');
+?>
+
 <div class="container">
   <div class="row">
   <?php
@@ -6,56 +10,17 @@
     while ( have_posts() ):
       the_post();
   ?>
-      <div class="article-bg" style="background-image: url(<?php bloginfo('template_directory'); ?>/images/big_test.jpg)"></div>
+    <article class="blog-post work-post">
+      <h2><?php the_title(); ?></h2>
 
-      <article class="splash container">
-          <div id="intro">
-            <h2><?php the_title(); ?></h2>
-
-            <ul>
-              <li><?php the_field( 'client' ); ?></li>
-              <li><?php the_field( 'project_type' ); ?></li>
-              <li><a href="<?php the_permalink() ;?>">Visit the site</a></li>
-            </ul>
-
-            <p><?php the_field( 'description' ); ?></p>
-          </div>
-      </article>
-
-    </div>
-
-    <div class="row white">
-      <div class="col-md-6 details">
-        <h2>Extra Stuff</h2>
-        <p>
-          Aenean suscipit mi non dolor consequat lobortis. Quisque at lorem at nibh tincidunt consequat.
-          Sed scelerisque turpis eu mattis pharetra. Fusce eget ante justo. Maecenas dignissim, tellus vel vulputate hendrerit,
-          erat justo pharetra lacus, sed rutrum enim lacus sollicitudin enim. Pellentesque eu suscipit dolor.
-          Nunc suscipit ultricies risus aliquet accumsan. Mauris facilisis tortor eget augue hendrerit, nec interdum mi pretium.
-          Aenean varius hendrerit metus ut faucibus.
-        </p>
+      <div class="info">
+        <?php the_time('F jS, Y'); ?> by <?php the_author( );?>
       </div>
 
-      <div class="col-md-6 detail">
-        <img src="<?php bloginfo('template_directory'); ?>/images/single_test.jpg">
+      <div class="main-text">
+        <?php the_content(); ?>
       </div>
-    </div>
-
-
-    <div class="action">
-      <img src="<?php bloginfo('template_directory'); ?>/images/big_test_last.png">
-    </div>
-
-    <div class="row white quote">
-      <h3><em>Client Testimonial - Very Brief</em></h3>
-    </div>
-
-     <div class="row">
-      <div id="skills">
-        <h3>Showing off our Skills in:</h3>
-        <canvas id="myChart" width="400" height="400"></canvas>
-      </div>
-    </div>
+    </article>
   <?php
     endwhile;
   endif;
