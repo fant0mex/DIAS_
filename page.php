1e9081a3ecfa26079c3c0d929d60b1644aa3ee15
pire <rob.mccormack@diascreative.com>
2014-02-27 11:14:17 +0000

    simply page styling

--- page.php
+++ page.php
@@ -5,10 +5,12 @@
 ?>
 
 <article class="container">
-  <?php if( !get_field('hero_image') ): ?>
-  <h2><?php the_title(); ?></h2>
-  <?php endif; ?>
-  <p><?php the_content(); ?></p>
+  <article class="page">
+    <?php if( !get_field('hero_image') ): ?>
+    <h2><?php the_title(); ?></h2>
+    <?php endif; ?>
+    <p><?php the_content(); ?></p>
+  </article>
 </article>
 
 <?php get_footer(); ?>
