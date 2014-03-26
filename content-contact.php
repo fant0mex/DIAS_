f08217486a6416addced84554001de71fd67d317
pire <rob.mccormack@diascreative.com>
2014-03-26 16:19:47 +0100

    migrate contact page to content-contact.php * some code is missing at the top and bottom of content-contact.php Joe, if you can see what's missing and amend so that the contact form is placed nicely in line 24

--- content-contact.php
+++ content-contact.php
@@ -1,48 +1,32 @@
 <?php
-  get_header();
-  the_post();
-  get_template_part('content', 'hero-image');
+ /*
+  * Add some php to get the info from the contact page
+  */
 ?>
-
 <div class="container-fluid" id="contact">
   <div class="row">
-    <div class="col-sm-8 col-sm-push-4">
-      <div id="address">
-        <h3>Address</h3>
-
-        <p>3rd Floor, 6-8 Bonhill Street, London, EC2A 4BX</p>
-        <p>Tel: <a href="callto:+442079935746">+44 (0) 20 7993 5746</a></p>
-        <p>Email: <a href="mailto:info@diascreative.com">info@diascreative.com</a></p>
-      </div>
-
+    <div class="col-sm-12">
       <div id="map"></div>
-      <a id="navigate"
-        target="_blank"
-        href="http://maps.google.com/maps?daddr=3rd+Floor,+6-8+Bonhill+Street,+London,+EC2A+4BX&amp;saddr=">
-        <i class="icon navigate"></i>
-      </a>
+        <a id="navigate"
+          target="_blank"
+          href="http://maps.google.com/maps?daddr=3rd+Floor,+6-8+Bonhill+Street,+London,+EC2A+4BX&amp;saddr=">
+          <i class="icon navigate"></i>
+        </a>
     </div>
-    <div class="col-sm-4 col-sm-pull-8" id="contact-form">
-      <div>
-        <h3 class="page-head">Talk to us</h3>
-        <?php the_content(); ?>
-      </div>
+    <div class="address">
+        <p>DIAS Creative<br>3rd Floor,<br>6-8 Bonhill Street<br>London, EC2A 4BX</p>
+        <h3>TALK TO US</h3>
+        <p><a href="callto:+442079935746">+44 (0) 20 7993 5746</a></p>
+        <p><a href="mailto:info@diascreative.com">info@diascreative.com</a></p>
     </div>
-  </div>
-</div>
 
-<div>
-  <div class="container">
-    <div class="row jobs">
-      <div class="col-sm-6">
-        <?php the_field('contact_page_left_box'); ?>
-      </div>
-
-      <div class="col-sm-6">
-        <?php the_field('contact_page_right_box'); ?>
-      </div>
+    <div>
+      <?php the_content(); ?>
     </div>
   </div>
 </div>
-
-<?php get_footer(); ?>
+<?php
+ /*
+  * Add some php to revert to the original query
+  */
+?>
--- front-page.php
+++ front-page.php
@@ -133,27 +133,8 @@
       <h3 class="join">FIND US</h3>
   </div>
 </div>
-<div class="container-fluid" id="contact">
-  <div class="row">
-    <div class="col-sm-12">
-      <div id="map"></div>
-        <a id="navigate"
-          target="_blank"
-          href="http://maps.google.com/maps?daddr=3rd+Floor,+6-8+Bonhill+Street,+London,+EC2A+4BX&amp;saddr=">
-          <i class="icon navigate"></i>
-        </a>
-    </div>
-    <div class="address">
-        <p>DIAS Creative<br>3rd Floor,<br>6-8 Bonhill Street<br>London, EC2A 4BX</p>
-        <h3>TALK TO US</h3>
-        <p><a href="callto:+442079935746">+44 (0) 20 7993 5746</a></p>
-        <p><a href="mailto:info@diascreative.com">info@diascreative.com</a></p>
-    </div>
-    <div>
-      <?php get_template_part( 'content','contact' ); ?>
-    </div>
-  </div>
-</div>
+
+<?php get_template_part( 'content','contact' ); ?>
 
 <div class="box-full-width shout-out">
   <img src="<?php bloginfo('template_directory'); ?>/images/microphone.jpg" />
