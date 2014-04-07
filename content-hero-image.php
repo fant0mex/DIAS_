f919e1d3072ffb8ccc756f15bad9b962757437e5
pire <rob.mccormack@diascreative.com>
2014-04-07 10:53:06 +0200

    only create "find out more about us" in homepage instead of creating everywhere and only showing in homepage

--- content-hero-image.php
+++ content-hero-image.php
@@ -11,7 +11,9 @@ if( $image ): ?>
     <div id="hero-caption">
       <div class="box-secondary">
         <?php the_field('hero_description');?>
-        <a class="show-me scroll" href="#case">Find Out More About Us</a>
+        <?php if(is_home() || is_front_page()): ?>
+        <div><a class="show-me scroll" href="#case">Find Out More About Us</a></div>
+        <?php endif; ?>
       </div>
      </div>
 
