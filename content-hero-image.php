b0ba16218168d4b743d0e0a26cd31808311b1c05
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-04-23 15:10:25 +0100

    Hero image resizing fix

--- content-hero-image.php
+++ content-hero-image.php
@@ -4,8 +4,6 @@ $image = get_field('hero_image');
 
 if( $image ): ?>
 <div id="hero" style="background-image: url(<?= $image['url']; ?>)">
-  <img src="<?= $image['url']; ?>" height="<?= $image['height'] ?>" width="<?= $image['width'] ?>"
-    <?= !get_field('hero_description') ? '' : ' class="no-text"' ?>>
 
   <?php if( get_field('video_loop') ): ?>
     <div class="video-container dias-video-filler">
