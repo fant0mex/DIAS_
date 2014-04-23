91726578f901a76d1ace1b9838e63ebcef276053
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-04-23 11:21:24 +0100

    amends contact form loop

--- content-contact.php
+++ content-contact.php
@@ -71,12 +71,12 @@
               <?php if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
                   <?php the_content(); ?>
+                  <?php endwhile; endif; ?>
         </div>
-            <?php endwhile; endif; ?>
       </div>
     </div>
   </div>
 </div>
-</div>
+
 
 
