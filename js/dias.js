7d18b4a3f2cc7eb804a22da4ca15066e6f8e0848
pire <rob.mccormack@diascreative.com>
2014-04-25 14:58:40 +0200

    include the slick JS in code kit, remove unnecessary stuff

--- js/dias.js
+++ js/dias.js
@@ -1,50 +1,6 @@
 (function ($, $win) {
   'use strict';
 
-  // var $map = $('#map'),
-  //     $chart = $("#myChart"),
-  //     $contact = $('#contact-form>div'),
-  //     touchDevice = 'ontouchstart' in window || 'onmsgesturechange' in window,
-  //     MAXWIDTH = 1160;
-
-  // if( touchDevice ) {
-  //   $('html').addClass('touch-device');
-  // }
-
-  // if( $map.length ) {
-  //   var map = L.map('map').setView([51.522755, -0.086504], 17);
-  //   var t, i, n, o, s, a = document.getElementsByTagName("script"),
-  //     r = /[\/^]dias-ck[\-\._]?([\w\-\._]*)\.js\??/;
-  //   for (t = 0, i = a.length; i > t; t++)
-  //     if (n = a[t].src, o = n.match(r)) s = n.split(r)[0], (s ? s + "/" : "");
-
-  //   L.Icon.Default.imagePath = s + "/libs/leaflet/images";
-
-  //   map.scrollWheelZoom.disable();
-
-  //   L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-  //       maxZoom: 18
-  //   }).addTo(map);
-
-  //   var marker = L.marker([51.522555, -0.085304]).addTo(map);
-  // }
-
-  // if( $contact.length ) {
-  //   // "complex" layout in contact page requires some JS
-  //   // to make sure the contact form is aligned left with content
-  //   // when the browser is wider than our max width
-  //   $win.resize(function() {
-  //     var left = 0,
-  //         sideMargin = $win.width() - MAXWIDTH;
-
-  //     if( sideMargin > 0 ) {
-  //       left = sideMargin / 2;
-  //     }
-
-  //     $contact.css('paddingLeft', left + 'px');
-  //   }).resize();
-  // }
-
   document.addEventListener("touchstart", function(){}, true);
 
   // Scroll funcitons ////////////////////////////////////////////////////////////////////////////
@@ -136,8 +92,6 @@
     });
   }
 
-  $(document).ready(function(){
-    $('.carousel').slick();
-  });
+  $('.carousel').slick();
 
 }(jQuery, jQuery(window)));
