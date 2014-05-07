32f0530b94c39f8ef1eab3764246a8a5f659ab08
Joe Hobson <joseph.j.hobson@googlemail.com>
2014-05-07 10:55:53 +0100

    Archive-work indentation/image cleanup

--- archive-work.php
+++ archive-work.php
@@ -24,13 +24,13 @@
           <a href="<?php the_permalink(); ?>">
             <?php $image = get_field('project_image_home'); ?>
               <img src="<?= $image['sizes']['large'] ?>">
-                <div>
-                  <h3><span><?php the_field( 'client' ); ?></span></h3>
-                    <p><span><?php the_field('project_blurb');?></span></p>
-                     <?php foreach($project_type as $item): ?>
-                      <button class="btn <?php echo strtolower($item); ?> single"><?php echo $item; ?></button>
-                     <?php endforeach; ?>
-                </div>
+              <div>
+                <h3><span><?php the_field( 'client' ); ?></span></h3>
+                <p><span><?php the_field('project_blurb');?></span></p>
+                <?php foreach($project_type as $item): ?>
+                <button class="btn <?php echo strtolower($item); ?> single"><?php echo $item; ?></button>
+                <?php endforeach; ?>
+              </div>
           </a>
       <?php }
         wp_reset_postdata();
