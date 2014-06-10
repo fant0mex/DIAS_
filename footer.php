b6228d16ae34ee71b74408ec44490d571915458f
pire <rob.mccormack@diascreative.com>
2014-06-10 17:33:22 +0200

    version bump

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
       </div>
     </footer>
 
-    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/dias-ck.js?v=2.2" defer></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/dias-ck.js?v=2.3" defer></script>
       <?php wp_footer(); ?>
   </body>
 </html>
